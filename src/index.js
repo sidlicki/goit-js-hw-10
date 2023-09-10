d181e95ee4b3310bcff4aac5a2f21e07c17226db
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-09-11 00:57:30 +0200

    fix select

--- src/index.js
+++ src/index.js
@@ -20,8 +20,7 @@ function addingOptions() {
   fetchBreeds()
     .then(catsList => {
       // Додаємо опції до селекту на основі отриманого списку порід
-      const defaultOption =
-        '<option value="default">Оберіть породу кота</option>';
+      const defaultOption = ' <option data-placeholder="true"></option>';
       selectCat.innerHTML =
         defaultOption +
         catsList.data
@@ -48,38 +47,29 @@ function addingOptions() {
 // Функція для відображення інформації про вибраного кота по айді
 function addingInformation(evt) {
   const idCat = evt.target.value;
-  if (idCat !== 'default') {
-    // Порівнюємо з рядком "default"
-    const defOpt = selectCat.querySelector('option[value="default"]');
-    if (defOpt) {
-      defOpt.remove();
-      //не можу зрозуміти як видаляти дефолтну опцію вибору
-    }
+  // Вимикаємо блок інформації та вмикаємо лоадер
+  offDisplayInfo();
+  onLoader();
 
-    // Вимикаємо блок інформації та вмикаємо лоадер
-    offDisplayInfo();
-    onLoader();
+  fetchCatByBreed(idCat)
+    .then(resp => {
+      const currentCat = resp.data[0];
+      const catImg = currentCat.url;
+      const catName = currentCat.breeds[0].name;
+      const catDescr = currentCat.breeds[0].description;
 
-    fetchCatByBreed(idCat)
-      .then(resp => {
-        const currentCat = resp.data[0];
-        const catImg = currentCat.url;
-        const catName = currentCat.breeds[0].name;
-        const catDescr = currentCat.breeds[0].description;
-
-        // Відображаємо інформацію про кота та вимикаємо лоадер
-        catInfo.innerHTML = ` <img class="cat-img" src="${catImg}" alt="${catName}">
+      // Відображаємо інформацію про кота та вимикаємо лоадер
+      catInfo.innerHTML = ` <img class="cat-img" src="${catImg}" alt="${catName}">
     <h2 class="cat-name">${catName}</h2>
     <p class="cat-descr">${catDescr}</p>`;
-        offLoader();
-        onDisplayInfo();
-      })
-      .catch(err => {
-        // Вимикаємо лоадер та показуємо повідомлення про помилку
-        offLoader();
-        onError();
-      });
-  }
+      offLoader();
+      onDisplayInfo();
+    })
+    .catch(err => {
+      // Вимикаємо лоадер та показуємо повідомлення про помилку
+      offLoader();
+      onError();
+    });
 }
 
 // Функції для зміни видимості лоадера та блоку інформації
@@ -112,11 +102,13 @@ function onError() {
 // Функція для ініціалізації кастомізації селекту
 function onSelect() {
   selectCat.style.display = 'flex';
+
   new SlimSelect({
     select: '#selectElement',
     settings: {
       showSearch: false,
       searchHighlight: true,
+      placeholderText: 'Оберіть породу кота',
     },
   });
 }
