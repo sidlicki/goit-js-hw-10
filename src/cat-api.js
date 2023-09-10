03c6f76fc9a0c8a5d05a56d1dde4ffd8a136a458
sidlicki <130383232+sidlicki@users.noreply.github.com>
2023-09-10 20:29:05 +0200

    на дз

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,19 +1,23 @@
 //live_Io2IL4uq7kBqpCSyUxyMmzmqLQlKVoUlsGVytGVZAbTWrdVebIluIG10KwbORlhm
 
+// Імпортуємо axios для здійснення запитів до API
 import axios from 'axios';
 
+// Встановлюємо заголовок 'x-api-key' для всіх запитів до API
 axios.defaults.headers.common['x-api-key'] =
   'live_Io2IL4uq7kBqpCSyUxyMmzmqLQlKVoUlsGVytGVZAbTWrdVebIluIG10KwbORlhm';
 
+// Функція для отримання списку порід котів
 function fetchBreeds() {
   return axios.get('https://api.thecatapi.com/v1/breeds').then(resp => {
     return resp;
   });
 }
 
+// Функція для отримання випадкового кота за айді породи
 function fetchCatByBreed(breedId) {
   return axios
-    .get(`https://api.thecatapi.com/v1/breeds/search?breed_ids=${breedId}`)
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
     .then(resp => {
       return resp;
     });
--- src/index.js
+++ src/index.js
@@ -1,36 +1,122 @@
+// Імпорт необхідних модулів з інших файлів
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
 
-const select = document.querySelector('.breed-select');
+// Отримуємо посилання на елементи DOM, які нам потрібні
+const selectCat = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
 
-onLoader();
+// Викликаємо функцію для наповнення опцій селекту
 addingOptions();
 
-//функція наповнення селекту
+// Встановлюємо початковий текст для блоку інформації про кота
+catInfo.innerHTML = `<p>Виберіть котика зі списку вище для відображення інформації про нього</p>
+`;
+
+// Функція для наповнення опцій селекту зі списком порід
 function addingOptions() {
-  fetchBreeds().then(
-    catsList =>
-      (select.innerHTML = catsList.data
-        .map(({ id, name }) => `<option value="${id}">${name}</option>`)
-        .join(''))
-  );
-  offLoader();
+  fetchBreeds()
+    .then(catsList => {
+      // Додаємо опції до селекту на основі отриманого списку порід
+      const defaultOption =
+        '<option value="default">Оберіть породу кота</option>';
+      selectCat.innerHTML =
+        defaultOption +
+        catsList.data
+          .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+          .join('');
+
+      // Додаємо слухача подій для селекту, щоб відображати інформацію при виборі опції
+      selectCat.addEventListener('change', addingInformation);
+
+      // Вмикаємо відображення блоку інформації та вимикаємо лоадер
+      onDisplayInfo();
+      offLoader();
+
+      // Ініціалізуємо функціонал SlimSelect для кастомізації селекту
+      onSelect();
+    })
+    .catch(err => {
+      // Вимикаємо лоадер та показуємо повідомлення про помилку
+      offLoader();
+      onError();
+    });
 }
 
-//функція наповнення інформації по вибраному айді
-function addingInformation() {}
+// Функція для відображення інформації про вибраного кота по айді
+function addingInformation(evt) {
+  const idCat = evt.target.value;
+  if (idCat !== 'default') {
+    // Порівнюємо з рядком "default"
+    const defOpt = selectCat.querySelector('option[value="default"]');
+    if (defOpt) {
+      defOpt.remove();
+      //не можу зрозуміти як видаляти дефолтну опцію вибору
+    }
 
-//функція для зміни видимості лоадера
+    // Вимикаємо блок інформації та вмикаємо лоадер
+    offDisplayInfo();
+    onLoader();
+
+    fetchCatByBreed(idCat)
+      .then(resp => {
+        const currentCat = resp.data[0];
+        const catImg = currentCat.url;
+        const catName = currentCat.breeds[0].name;
+        const catDescr = currentCat.breeds[0].description;
+
+        // Відображаємо інформацію про кота та вимикаємо лоадер
+        catInfo.innerHTML = ` <img class="cat-img" src="${catImg}" alt="${catName}">
+    <h2 class="cat-name">${catName}</h2>
+    <p class="cat-descr">${catDescr}</p>`;
+        offLoader();
+        onDisplayInfo();
+      })
+      .catch(err => {
+        // Вимикаємо лоадер та показуємо повідомлення про помилку
+        offLoader();
+        onError();
+      });
+  }
+}
+
+// Функції для зміни видимості лоадера та блоку інформації
 function onLoader() {
-  loader.style.display = 'block';
+  loader.style.display = 'flex';
 }
 function offLoader() {
   loader.style.display = 'none';
 }
-//функція для виклику сповіщення про помилку
+function onDisplayInfo() {
+  catInfo.style.display = 'flex';
+}
+function offDisplayInfo() {
+  catInfo.style.display = 'none';
+}
+
+// Функція для виклику сповіщення про помилку
 function onError() {
-  Notiflix.Report('Oops! Something went wrong! Try reloading the page!');
+  Notiflix.Notify.failure(
+    'Oops! Something went wrong! Try reloading the page!',
+    {
+      position: 'center-top',
+      timeout: 10000,
+      width: '350px',
+      fontSize: '20px',
+    }
+  );
+}
+
+// Функція для ініціалізації кастомізації селекту
+function onSelect() {
+  selectCat.style.display = 'flex';
+  new SlimSelect({
+    select: '#selectElement',
+    settings: {
+      showSearch: false,
+      searchHighlight: true,
+    },
+  });
 }
